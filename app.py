d6b5856d5b01c50e8461a3c1d830807107665958
Harsh Raj <harshmishra142000@gmail.com>
2026-01-08 00:20:30 +0530

    testing

--- app.py
+++ app.py
@@ -1609,6 +1609,10 @@ async def download_excel():
 def index():
     return render_template("index.html")
 
+@app.route("/health")  # <- Separate health endpoint
+def health_check():
+    return {"healthy":true,"status":"IOC Lookup GCP v1.0"}
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)  # Adjust port and debug as needed
