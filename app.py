7061891385b15d36a1fc7ca9c81d992e839dffec
Harsh Raj <harshmishra142000@gmail.com>
2026-08-26 11:59:07 +0530

    added about route

--- app.py
+++ app.py
@@ -1498,7 +1498,13 @@ def index():
         "index.html"
     )
 
+@app.route("/about")
+def about():
 
+    return render_template(
+        "about.html"
+    )
+    
 @app.route("/ping")
 def ping():
 
